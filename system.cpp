f5fe427a9c3ad991f31628880912c101cf2c141a
olivier st-laurent <kzaurckichz@gmail.com>
2019-08-13 23:57:39 -0400

    ..

---
+++ system.cpp
@@ -0,0 +1,6 @@
+#include <v4d.h>
+
+V4DSYSTEM int test1(int b) {
+	return b * 2;
+}
+
---
+++ tests.cxx
@@ -0,0 +1,24 @@
+#include <v4d.h>
+
+namespace v4d::tests {
+	int SYSTEMS_SAMPLE_1() {
+
+		v4d::SystemsLoader systemsLoader;
+
+		auto sampleSystem = systemsLoader.LoadSystem("sample");
+		if (!sampleSystem) {
+			LOG_ERROR("ERROR : Could not load sample system library")
+			return 1;
+		}
+
+		LOAD_DLL_FUNC(sampleSystem, int, test1, int)
+		
+		int res = test1(4);
+		if (res != 8) {
+			LOG_ERROR("ERROR : test1 from sample system returned " << res << " instead of 8")
+			return 2;
+		}
+
+		return 0;
+	}
+}

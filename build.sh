c65ff9c4b1ed282a182754d7f6a1e88f8c8b6ff1
olivier st-laurent <kzaurckichz@gmail.com>
2019-08-15 16:48:26 -0400

    updated standards

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ OUTPUT_NAME="$SYSTEM_NAME"
 
 # Build Modes
 if [ $MODE == "RELEASE" ] ; then
-  OUTPUT_DIR="$PROJECT_DIR/build/release/systems/$SYSTEM_NAME"
-  OPTIONS="-O3 -D_RELEASE"
+	OUTPUT_DIR="$PROJECT_DIR/build/release/systems/$SYSTEM_NAME"
+	OPTIONS="-O3 -D_RELEASE"
 else
-  MODE='DEBUG'
-  OUTPUT_DIR="$PROJECT_DIR/build/debug/systems/$SYSTEM_NAME"
-  OPTIONS="-ggdb -g -O0 -D_DEBUG"
+	MODE='DEBUG'
+	OUTPUT_DIR="$PROJECT_DIR/build/debug/systems/$SYSTEM_NAME"
+	OPTIONS="-ggdb -g -O0 -D_DEBUG"
 fi
 mkdir -p "$OUTPUT_DIR"
 
@@ -43,10 +43,10 @@ COMMAND_WINDOWS="x86_64-w64-mingw32-g++ \
 	-shared -Wl,-soname,$OUTPUT_NAME.dll \
 	-o $OUTPUT_DIR/$OUTPUT_NAME.dll \
 	-D_WINDOWS \
-    -lwinpthread \
-    -lstdc++ \
-    -lgcc \
-    -static -static-libgcc -static-libstdc++ \
+	-lwinpthread \
+	-lstdc++ \
+	-lgcc \
+	-static -static-libgcc -static-libstdc++ \
 	$GLOBAL_COMPILER_FLAGS \
 "
 #-Wl,-rpath,../.. \

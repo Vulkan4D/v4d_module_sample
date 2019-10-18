6ea59f66b308cca69972cf9958cff0ca75ac03fb
olivier st-laurent <kzaurckichz@gmail.com>
2019-10-17 21:11:32 -0400

    Refactoring for Precompiled headers

--- module.cpp
+++ module.cpp
@@ -46,6 +46,10 @@
 #define THIS_MODULE_REVISION 1
 #define THIS_MODULE_DESCRIPTION "Sample V4D Module"
 
+// Pre-compiled common header
+#include <common/pch.hh>
+
+// V4D Core Header
 #include <v4d.h>
 
 ////////////////////////////////////////////////////////////////////

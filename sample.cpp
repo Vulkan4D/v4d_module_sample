cb883ffcabe6cbeaea4329f8ae875680b6f95cff
Olivier St-Laurent <kzaurckichz@gmail.com>
2020-11-10 04:17:09 -0500

    Refactoring module classes

--- sample.cpp
+++ sample.cpp
@@ -3,15 +3,15 @@
 
 v4d::graphics::Window* window = nullptr;
 
-V4D_MODULE_CLASS(V4D_Input) {
+V4D_MODULE_CLASS(V4D_Mod) {
 	
-	V4D_MODULE_FUNC(std::string, CallbackName) {return THIS_MODULE;}
+	V4D_MODULE_FUNC(std::string, InputCallbackName) {return THIS_MODULE;}
 	
-	V4D_MODULE_FUNC(void, Init, v4d::graphics::Window* w, v4d::graphics::Renderer*, v4d::scene::Scene*) {
+	V4D_MODULE_FUNC(void, InitWindow, v4d::graphics::Window* w) {
 		window = w;
 	}
 	
-	V4D_MODULE_FUNC(void, KeyCallback, int key, int scancode, int action, int mods) {
+	V4D_MODULE_FUNC(void, InputKeyCallback, int key, int scancode, int action, int mods) {
 		if (action != GLFW_RELEASE
 			#ifdef _ENABLE_IMGUI
 				&& (!ImGui::IsAnyWindowFocused() || key == GLFW_KEY_ESCAPE)
